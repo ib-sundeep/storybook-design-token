dd18a17c6c8757a08fe5c52c39b7b1ee6b914c02
Philipp Siekmann <philipp.siekmann@uxi.de>
2021-11-26 09:26:23 +0100

    add simple webpack 5 demo project

---
+++ demo-webpack5/.storybook/preview.js
@@ -0,0 +1,24 @@
+const tokenContext = require.context(
+  '!!raw-loader!..',
+  true,
+  /.\.(css|less|scss|svg)$/
+);
+
+const tokenFiles = tokenContext.keys().map(function (filename) {
+  return { filename: filename, content: tokenContext(filename).default };
+});
+
+console.log(tokenFiles);
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  },
+  designToken: {
+    files: tokenFiles
+  }
+};
